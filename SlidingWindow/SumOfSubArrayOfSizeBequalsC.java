a8faa067a6874cfd94bb25fe6c3defd3ad369906
SELVADHARANI K <65858598+selvadharani1@users.noreply.github.com>
2024-11-18 11:10:55 +0530

    Create SumOfSubArrayOfSizeBequalsC.java

---
+++ SlidingWindow/SumOfSubArrayOfSizeBequalsC.java
@@ -0,0 +1,24 @@
+public class SlidingWindow {
+    public int solve(ArrayList<Integer> A, int B, int C) {
+        int sum=0;
+        for(int i=0;i<B;i++)
+        {
+        sum+=A.get(i);
+        }
+        if(sum==C)
+        {
+            return 1;
+        }
+        for(int i=B;i<A.size();i++)
+        {
+            sum=sum-A.get(i-B);
+            sum+=A.get(i);
+         //   System.out.print(sum+"*");
+            if(sum==C)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
