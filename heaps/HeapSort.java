222a023a3ca11a090b0e212a4a143a926eb18847
SELVADHARANI K <65858598+selvadharani1@users.noreply.github.com>
2024-11-25 01:33:20 +0530

    Create HeapSort.java
    Heap sort with heapify techinque

---
+++ heaps/HeapSort.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public ArrayList<Integer> heapSort(ArrayList<Integer> A) {
+        int swapper=0;
+         for(int i=A.size()/2-1;i>=0;i--){
+       heapify(A,i,A.size());
+         }
+       //   System.out.print(A);
+      for(int i=A.size()-1;i>0;i--){
+      swapper = A.get(0);
+      A.set(0,A.get(i));
+      A.set(i,swapper);
+       heapify(A,0,i);
+      }
+      return A;
+    }
+
+    public void heapify(ArrayList<Integer> array,int root,int size)
+    {
+        int left=root*2+1;
+        int right=left+1;
+        int max=root;
+        int swapper=0;
+        if(left<size&&array.get(left)>array.get(max))
+        {
+            max=left;
+        }
+        if(right<size&&array.get(right)>array.get(max))
+        {
+            max=right;
+        }
+        if(max!=root)
+        {
+        swapper=array.get(max);
+        array.set(max,array.get(root));
+        array.set(root,swapper);
+        heapify(array,max,size);
+        }
+          }
+    }
+
