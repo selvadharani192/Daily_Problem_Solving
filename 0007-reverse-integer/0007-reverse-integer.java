b791e9c95fea10843401d9327614b998398729bd
SELVADHARANI K <65858598+selvadharani192@users.noreply.github.com>
2025-10-03 23:32:39 +0530

    Time: 1 ms (81.25%), Space: 40.7 MB (94.77%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse=0;
+        int manipulationVariable=x;
+        if(x<0){
+            manipulationVariable=manipulationVariable*-1;
+        }
+        while(manipulationVariable>0){
+            try{
+          reverse=Math.addExact(Math.multiplyExact(reverse,10),manipulationVariable%10);
+            }
+            catch(Exception e){
+                return 0;
+            }
+          manipulationVariable=manipulationVariable/10;
+        }
+          if(x<0){
+               reverse=reverse*-1;
+          }
+        return reverse;
+    }
+}
