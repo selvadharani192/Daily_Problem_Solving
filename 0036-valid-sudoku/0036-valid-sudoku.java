f8ef9d442afd07c7e1bac894b7a37e7193e05ed8
SELVADHARANI K <65858598+selvadharani192@users.noreply.github.com>
2026-07-08 07:54:36 +0530

    Time: 3 ms (56.51%), Space: 46.5 MB (47.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> tracker = new HashSet<String>();
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] column = new HashSet[9];
+        HashSet<Character>[] box = new HashSet[9];
+        for(int i=0;i<9;i++){
+            row[i] = new HashSet<Character>();
+             column[i] = new HashSet<Character>();
+              box[i] = new HashSet<Character>();
+        }
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
-                    continue;
-                }
-                String row = board[i][j]+"-1"+i;
-                 String column = board[i][j]+"-2"+j;
-                  String box = board[i][j]+"-3"+i/3+"."+j/3;
-                  if(tracker.contains(row)||tracker.contains(column)||tracker.contains(box)){
+                                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if(board[i][j]=='.'){continue;}
+                if(row[i].contains(board[i][j])){
                     return false;
-                  }
-                  tracker.add(row);
-                   tracker.add(column);
-                    tracker.add(box);
+                }
+                if(column[j].contains(board[i][j])){
+                    return false;
+                }
+
+                if(box[boxIndex].contains(board[i][j])){
+                    return false;
+                }
+                row[i].add(board[i][j]);
+                column[j].add(board[i][j]);
+                box[boxIndex].add(board[i][j]);
             }
         }
-        return true;
+       return true;
     }
 }
