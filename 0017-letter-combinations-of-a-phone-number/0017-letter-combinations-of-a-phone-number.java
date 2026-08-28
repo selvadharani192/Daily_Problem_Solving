18c1b36548b2a96fcf00244ee97679a72bb7f7d1
SELVADHARANI K <65858598+selvadharani192@users.noreply.github.com>
2026-08-28 22:45:26 +0530

    Time: 3 ms (32.56%), Space: 48.8 MB (25.9%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    private static Map<Character,String> dialPad = new HashMap<Character,String>();
+
+    public List<String> letterCombinations(String digits) {
+        dialPad.put('2',"abc");
+        dialPad.put('3',"def");
+        dialPad.put('4',"ghi");
+        dialPad.put('5',"jkl");
+        dialPad.put('6',"mno");
+        dialPad.put('7',"pqrs");
+        dialPad.put('8',"tuv");
+        dialPad.put('9',"wxyz");
+
+        List<String> result = new ArrayList<String>();
+        generatePossibilities(0,"",digits,result);
+        return result;
+    }
+    public void generatePossibilities(int index,String combination,String digits,List<String> combinations){
+        if(combination.length()==digits.length()){
+           combinations.add(combination);
+           return;
+        }
+        if(index>=digits.length()){
+            return;
+        }
+        char c= digits.charAt(index);
+        String dialPadDigits = dialPad.get(c);
+        for(char ch:dialPadDigits.toCharArray()){
+            generatePossibilities(index+1,combination+ch,digits,combinations);
+        }
+    }
+}
